5937c8b119e1a4edbaa123fbb13c9f31a64e3b4c
Phuoc Nguyen <100539667+Phuoc-Nguyen-CS@users.noreply.github.com>
2026-03-10 17:00:41 -0700

    small issue fixed

--- src/utils/terminal.ts
+++ src/utils/terminal.ts
@@ -1,5 +1,4 @@
 // src/utils/terminal.ts
-import { useEffect } from 'react'; // Make sure to import this at the top
 
 export const fireCommand = (cmd: string) => {
     window.dispatchEvent(new CustomEvent('run-cmd', { detail: cmd }));
